a3594320fab8507bd6417cb8735b166d497e4b4d
Papri <papri.sen2020@gmail.com>
2025-07-20 14:54:41 +0530

    Create Exp4_1_Bitwise_operarors.cpp

---
+++ Exp4_1_Bitwise_operarors.cpp
@@ -0,0 +1,30 @@
+// PAPRI SEN
+// 24070123070
+// A3
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 13;
+    int b = 14;
+    int bitwise_and,bitwise_or,bitwise_not_a,bitwise_not_b,bitwise_xor,left_shift_a,right_shift_a,left_shift_b,right_shift_b;
+    bitwise_and = a&b;
+    bitwise_or = a|b;
+    bitwise_not_a = ~a;
+    bitwise_not_b = ~b;
+    bitwise_xor = a^b;
+    left_shift_a = a << 1;
+    right_shift_a = a >> 2;
+    left_shift_b = b << 2;
+    right_shift_b = b >> 2;
+    cout << "AND: " << bitwise_and << endl;
+    cout << "OR: " << bitwise_or << endl;
+    cout << "NOT a: " << bitwise_not_a << endl;
+    cout << "NOT b: " << bitwise_not_b << endl;
+    cout << "XOR: " << bitwise_xor << endl;
+    cout << "leftshift a: " << left_shift_a << endl;
+    cout << "leftshift b: " << left_shift_b << endl;
+    cout << "rightshift a: " << right_shift_a << endl;
+    cout << "rightshift b: " << right_shift_b << endl;
+    return 0;
+}
